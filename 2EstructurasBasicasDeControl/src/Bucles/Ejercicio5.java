3fb33257a8b0374d1058f4f533c29a8305c6e1a9
darge <alexander.br159@gmail.com>
2022-03-23 20:38:11 +0100

    bucle 20

--- 2EstructurasBasicasDeControl/src/Bucles/Ejercicio5.java
+++ 2EstructurasBasicasDeControl/src/Bucles/Ejercicio5.java
@@ -32,7 +32,7 @@ public class Ejercicio5 {
 			System.out.println("introduce un numero");
 			num=tcl.nextInt();
 			
-		} while ((num<1)||(num>10));
+		} while ((num<1)||(num>20));
 		
 		
 		tcl.close();
